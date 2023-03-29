5768da38eb87f22822161a02a31f59cfc31c9e28
adambor <adamborteam@gmail.com>
2023-03-29 15:52:30 +0200

    Bump sollightning-sdk version

--- src/FEConstants.js
+++ src/FEConstants.js
@@ -1,17 +1,18 @@
 import BigNumber from "bignumber.js";
+import { SwapType } from "sollightning-sdk";
 export const FEConstants = {
     // expirySecondsBTCLNtoSol: 1*86400, //1 days
     // expirySecondsSoltoBTCLN: 3*86400, //3 days
     // confirmationsSoltoBTC: 3,
     // confirmationTargetSoltoBTC: 3,
-    // url: "https://node3.gethopa.com",
-    // customPorts: {
-    //     [SwapType.BTCLN_TO_SOL]: 34000,
-    //     [SwapType.SOL_TO_BTCLN]: 34001,
-    //     [SwapType.BTC_TO_SOL]: 34002,
-    //     [SwapType.SOL_TO_BTC]: 34003,
-    // },
-    url: "http://localhost",
-    customPorts: null,
+    url: "https://node3.gethopa.com",
+    customPorts: {
+        [SwapType.BTCLN_TO_SOL]: 34000,
+        [SwapType.SOL_TO_BTCLN]: 34001,
+        [SwapType.BTC_TO_SOL]: 34002,
+        [SwapType.SOL_TO_BTC]: 34003,
+    },
+    // url: "http://localhost",
+    // customPorts: null,
     satsPerBitcoin: new BigNumber(100000000)
 };
